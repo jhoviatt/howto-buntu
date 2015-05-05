a3bf98c08ffb39c468f57fbbe7ad5c1265fbb0b0
Jared Henry Oviatt <jaredhenryoviatt@gmail.com>
2015-05-05 01:32:35 -0700

    install script added for ghow

---
+++ ghow_install.sh
@@ -0,0 +1,47 @@
+
+#!/bin/bash
+
+# ghow install script
+
+# enter root
+echo 'Root required...'
+sudo echo '...' 
+
+# add aliases
+if [ 'alias | grep googlehowto | wc' != 0 ]; then
+  echo "googlehowto alias exists"
+else
+  echo "error noted, creating alias..."
+  echo "# created by howto-buntu (https://github.com/underscorejho/howto-buntu)" >> ~/.bashrc
+  echo "alias googlehowto='ghow.py'" >> ~/.bashrc
+fi
+
+if [ 'alias | grep ghow | wc' != 0 ]; then
+  echo "ghow alias exists"
+else
+  echo "error noted, creating alias..."
+  echo "# created by howto-buntu (https://github.com/underscorejho/howto-buntu)" >> ~/.bashrc
+  echo "alias ghow='googlehowto'" >> ~/.bashrc
+fi
+
+# add executable permissions
+chmod 744 ghow/ghow.py
+
+# put in ~/bin CHECK ~/bin is in PATH and CHECK ~bin is a directory
+if [[ "$PATH" == ?(*:)"$HOME/bin"?(:*) ]]; then
+  echo "adding ghow to $HOME/bin"
+  cp ghow/ghow.py $HOME/bin/ghow.py ###
+else 
+  if [-d $HOME/bin]; then
+  echo "..."
+  else
+    echo "$HOME/bin not found..."
+    echo "creating $HOME/bin..."
+    mkdir $HOME/bin ###
+  fi
+  echo "$HOME/bin not in $PATH..."
+  echo "adding $HOME/bin to $PATH..."
+  $PATH = $PATH:$HOME/bin ###
+  echo "copying ghow.py to $HOME/bin/ghow.py"
+  cp ghow/ghow.py $HOME/bin/ghow.py ###
+fi
